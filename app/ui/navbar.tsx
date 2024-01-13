9af10383844d1fafe79503efdd94994f106d4f07
Marton Bocsi <mvbocsi@gmail.com>
2024-01-12 20:53:13 -0600

    Set up foundations for steam market app

---
+++ app/ui/navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <div>
+      <div className="bg-scroll bg-gradient-to-b from-black to-transparent h-32 z-10 top-0 w-full absolute"></div>
+      <div></div>
+    </div>
+  );
+}
