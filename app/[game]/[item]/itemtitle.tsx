9c84e39de7961aed1b6d81d6b50a66f34ac4c9f4
Marton Bocsi <mvbocsi@gmail.com>
2024-02-03 22:22:00 -0600

    Added day change to item

--- app/[game]/[item]/itemtitle.tsx
+++ app/[game]/[item]/itemtitle.tsx
@@ -1,14 +1,22 @@
 "use client";
 
-import { getItemCurrent } from "@/app/lib/requests";
+import { getItemCurrent, getItemHistory2 } from "@/app/lib/requests";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function ItemTitle(props: any) {
   const { item, app, timestamp } = props;
-  console.log(props);
-  const [priceOverview, setPriceOverview] = useState(null);
+  // console.log(props);
+
+  const [priceOverview, setPriceOverview] = useState<{
+    lowest_price: string;
+    volume: number;
+  } | null>(null);
+
+  const [priceHistory, setPriceHistory] = useState<
+    [[string, number, string]] | null
+  >(null);
   const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -18,7 +26,35 @@ export default function ItemTitle(props: any) {
         setLoading(false);
       }
     );
+    getItemHistory2(
+      app?.appId,
+      encodeURIComponent(item?.itemHashName),
+      true
+    ).then((data) => {
+      setPriceHistory(data.slice(data.length - 30, data.length));
+    });
   }, [item, app]);
+  let close;
+  if (priceHistory) {
+    const cur_date = new Date();
+    cur_date.setHours(0, 0, 0, 0);
+    // console.log(cur_date);
+    close = priceHistory.find((point) => {
+      const this_date = new Date(point[0]);
+      // console.log(this_date);
+      return cur_date.getTime() === this_date.getTime();
+    });
+  }
+  let day_diff;
+  if (priceOverview && close) {
+    day_diff = [
+      (100 *
+        (parseFloat(priceOverview["lowest_price"].substring(1)) - close[1])) /
+        close[1],
+      parseFloat(priceOverview["lowest_price"].substring(1)) - close[1],
+    ];
+  }
+  // console.log(close);
   return (
     <div className="flex flex-row items-center gap-10">
       <div className="aspect-square flex items-center bg-slate-200 rounded-full p-4">
@@ -43,17 +79,34 @@ export default function ItemTitle(props: any) {
             {isLoading ? "Loading" : app.appName}
           </p>
         </Link>
-        <div className="flex flex-row align-bottom">
+        <div className="flex flex-row place-items-baseline">
           <p className="text-6xl font-semibold">
-            {priceOverview ? priceOverview["lowest_price"] : "Loading"}
+            {priceOverview
+              ? priceOverview["lowest_price"].substring(1)
+              : "Loading"}
           </p>
-          <div className="flex flex-col justify-end">
-            <p className="opacity-50 ml-2">
-              {priceOverview ? priceOverview["volume"] : "Unknown"} sold in last
-              24 hours
+          <p className="mx-2 text-lg opacity-50 font-semibold">USD</p>
+          {day_diff ? (
+            <p
+              className={`ml-2 font-semibold text-3xl ${
+                day_diff[0] > 0 ? "text-green-600" : "text-red-400"
+              }`}
+            >
+              {`${
+                (day_diff[1] > 0 ? "+" : "") + day_diff[1].toFixed(2).toString()
+              } ${
+                (day_diff[0] > 0 ? "+" : "") + day_diff[0].toFixed(2).toString()
+              }`}
+              %
             </p>
-          </div>
+          ) : (
+            <></>
+          )}
         </div>
+        <p className="opacity-50">
+          {priceOverview ? priceOverview["volume"] : "Unknown"} sold in last 24
+          hours
+        </p>
         <p className="opacity-50 font-semibold">
           At {timestamp.toLocaleString()}
         </p>
--- app/[game]/[item]/price/page.tsx
+++ app/[game]/[item]/price/page.tsx
@@ -1,8 +1,16 @@
 import { getItemHistory2 } from "@/app/lib/requests";
 
-export default async function Page() {
-  const history = await getItemHistory2(730, "Falchion%20Case");
-  const last = history.slice(history.length - 10, history.length);
+export default async function Page({
+  params,
+}: {
+  params: {
+    game: string;
+    item: string;
+  };
+}) {
+  const { game, item } = params;
+  const history = await getItemHistory2(parseInt(game), item);
+  const last = history.slice(history.length - 10, history.length).reverse();
   return (
     <div className="flex flex-col p-4 w-full items-center">
       <p className="text-3xl">Prices</p>
