5d9a85fa32e8e0b4efd59d32524afc647f5cce47
Marton Bocsi <mvbocsi@gmail.com>
2024-02-03 23:39:28 -0600

    Added template for more info in item summary page

--- app/[game]/[item]/itemsummary.tsx
+++ app/[game]/[item]/itemsummary.tsx
@@ -1,4 +1,4 @@
-import { getItemHistory2 } from "@/app/lib/requests";
+import { getItemCurrent, getItemHistory2 } from "@/app/lib/requests";
 
 export default async function ItemSummary({
   itemHash,
@@ -10,6 +10,10 @@ export default async function ItemSummary({
   let priceHistory: [[string, number, number]] | null = null;
   try {
     priceHistory = await getItemHistory2(appId, itemHash);
+  } catch {}
+  let priceOverview: { lowest_price: number } | null = null;
+  try {
+    priceOverview = await getItemCurrent(appId, itemHash);
   } catch {}
 
   let close;
@@ -59,30 +63,66 @@ export default async function ItemSummary({
       Math.max(acc[1], current[1]),
       current[2],
     ]);
+    if (priceOverview) {
+      if (year_max[1] < priceOverview["lowest_price"]) {
+        year_max[1] = priceOverview["lowest_price"];
+      } else if (year_min[1] > priceOverview["lowest_price"]) {
+        year_min[1] = priceOverview["lowest_price"];
+      }
+      if (day_max[1] < priceOverview["lowest_price"]) {
+        day_max[1] = priceOverview["lowest_price"];
+      } else if (day_min[1] > priceOverview["lowest_price"]) {
+        day_min[1] = priceOverview["lowest_price"];
+      }
+    }
   }
   return (
-    <div className="flex flex-col p-4">
-      <div className="p-4 flex flex-row justify-between">
-        <p>PREVIOUS CLOSE</p>
-        <p>{close ? "$" + close[1].toFixed(2).toString() : "$xx.xx"}</p>
+    <div className="flex flex-row p-4">
+      <div className="flex flex-col w-full">
+        <div className="p-4 flex flex-row justify-between">
+          <p>PREVIOUS CLOSE</p>
+          <p>{close ? "$" + close[1].toFixed(3).toString() : "$xx.xx"}</p>
+        </div>
+        <div className="p-4 border-t-2 flex flex-row justify-between">
+          <p>DAY RANGE</p>
+          <p>
+            {day_min ? "$" + day_min[1].toFixed(3).toString() : "$xx.xx"} -{" "}
+            {day_max ? "$" + day_max[1].toFixed(3).toString() : "$xx.xx"}
+          </p>
+        </div>
+        <div className="p-4 border-t-2 flex flex-row justify-between">
+          <p>54 WEEK RANGE</p>
+          <p>
+            {year_min ? "$" + year_min[1].toFixed(3).toString() : "$xx.xx"} -{" "}
+            {year_max ? "$" + year_max[1].toFixed(3).toString() : "$xx.xx"}
+          </p>
+        </div>
+        <div className="p-4 border-t-2 flex flex-row justify-between">
+          <p>MARKET CAP</p>
+          <p>$xxxxx</p>
+        </div>
       </div>
-      <div className="p-4 border-t-2 flex flex-row justify-between">
-        <p>DAY RANGE</p>
-        <p>
-          {day_min ? "$" + day_min[1].toFixed(2).toString() : "$xx.xx"} -{" "}
-          {day_max ? "$" + day_max[1].toFixed(2).toString() : "$xx.xx"}
-        </p>
-      </div>
-      <div className="p-4 border-t-2 flex flex-row justify-between">
-        <p>54 WEEK RANGE</p>
-        <p>
-          {year_min ? "$" + year_min[1].toFixed(2).toString() : "$xx.xx"} -{" "}
-          {year_max ? "$" + year_max[1].toFixed(2).toString() : "$xx.xx"}
-        </p>
-      </div>
-      <div className="p-4 border-t-2 flex flex-row justify-between">
-        <p>MARKET CAP</p>
-        <p>$xxxxx</p>
+      <div className="flex flex-col w-full">
+        <div className="p-4 flex flex-row justify-between">
+          <p>1 MONTH</p>
+          <p>$xxxxx</p>
+        </div>
+        <div className="p-4 border-t-2 flex flex-row justify-between">
+          <p>3 MONTH</p>
+          <p>$xxxxx</p>
+        </div>
+        <div className="p-4 border-t-2 flex flex-row justify-between">
+          <p>1 YEAR</p>
+          <p>$xxxxx</p>
+        </div>
+        <div className="p-4 border-t-2 flex flex-row justify-between">
+          <p>3 YEAR</p>
+          <p>$xxxxx</p>
+        </div>
+        <div className="p-4 border-t-2 flex flex-row justify-between">
+          <p>ALL TIME</p>
+          <p>$xxxxx</p>
+        </div>
       </div>
     </div>
   );
